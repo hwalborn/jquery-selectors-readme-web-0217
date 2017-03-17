114aa5a7da839cc7a4e456567e91e324eb07436d
hwalborn <hwalborn@gmail.com>
2017-03-17 14:18:11 -0400

    finally some tests

--- js/selectors.js
+++ js/selectors.js
@@ -1,1 +1,24 @@
 // declare your functions here...
+// const paragraphSelector = () => {
+//   return $('p')
+// };
+
+function paragraphSelector(){
+  return $('p')
+}
+
+var lastImageSelector = () => {
+  return $('img:last')
+}
+
+var ninjaBabySelector = () => {
+  return $('#baby-ninja')
+}
+
+var divSelector = () => {
+  return $('.pics')
+}
+
+var firstListItem = () => {
+  return $('ul#pic-list li:first-child')
+}
